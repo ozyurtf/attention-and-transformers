9fd223021fd95e74cbc0d39e8e8930bec6805c08
ozyurtf <ozyurtf@outlook.com>
2023-10-29 20:39:53 -0400

    updated the model architecture and optimization process

--- utils.py
+++ utils.py
@@ -1,56 +1,65 @@
 import torch
 import torch.nn as nn
+from einops import rearrange, repeat
 
 class PatchEmbedding(nn.Module):
     def __init__(self, image_size, patch_size, in_channels, embed_dim, batch_size):
       super(PatchEmbedding, self).__init__()
-      num_of_patches = (image_size * image_size)/(patch_size * patch_size)
-
       self.conv = nn.Conv2d(in_channels = in_channels,
                             out_channels = embed_dim,
                             kernel_size = (patch_size, patch_size),
-                            stride = (patch_size, patch_size))
-      self.flatten = nn.Flatten(2)
-      self.class_token_embedding = nn.Parameter(torch.randn(batch_size, 1, embed_dim))
-      self.position_embedding = nn.Parameter(torch.randn(batch_size, num_patches + 1, embed_dim))
+                            stride = (patch_size, patch_size),
+                            padding = (patch_size, patch_size))
+
+      self.relu = nn.ReLU()
+      self.maxpool = nn.MaxPool2d(kernel_size=patch_size, stride=1)
+      self.batch_norm = nn.BatchNorm2d(embed_dim)
 
     def forward(self, x):
       x = self.conv(x)
-      x = self.flatten(x).transpose(1, 2)
-      x = torch.cat([self.class_token_embedding, x], dim = 1)
-      x = x + self.position_embedding
-
+      x = self.relu(x)
+      x = self.maxpool(x)
+      x = self.batch_norm(x)
       return x
-
 
 class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, embed_dim, num_heads):
-      super(MultiHeadSelfAttention, self).__init__()
-      self.keys = nn.Linear(embed_dim, embed_dim)
-      self.queries = nn.Linear(embed_dim, embed_dim)
-      self.values = nn.Linear(embed_dim, embed_dim)
-      self.multi_head_attention = nn.MultiheadAttention(embed_dim,
-                                                        num_heads,
-                                                        batch_first = True)
+    def __init__(self, embed_dim, num_heads = 8, dropout = 0.01):
+        super().__init__()
+        self.num_heads = num_heads
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(embed_dim, num_heads * 3, bias = False)
+
+        self.to_out = nn.Sequential(nn.Linear(num_heads, embed_dim),
+                                    nn.Dropout(dropout))
 
     def forward(self, x):
-      k = self.keys(x)
-      q = self.queries(x)
-      v = self.values(x)
-      x = self.multi_head_attention(k, q, v)
-      return x
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d',
+                                          h = self.num_heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2))
+
+        mask = torch.eye(dots.shape[-1], device = dots.device, dtype = torch.bool)
+        mask_value = -torch.finfo(dots.dtype).max
+        dots = dots.masked_fill(mask, mask_value)
+
+        attn = self.attend(dots)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+    
 
 class TransformerBlock(nn.Module):
     def __init__(self, embed_dim, num_heads, mlp_dim, dropout, num_classes):
       super(TransformerBlock, self).__init__()
+
       # Layer Normalization
       self.layer_norm1 = nn.LayerNorm(embed_dim)
 
       # Multi Head Attention
       self.multi_head_attention1 = MultiHeadSelfAttention(embed_dim, num_heads)
-
-      # Dropout
-      self.dropout = nn.Dropout(dropout)
 
       # Layer Normalization
       self.layer_norm2 = nn.LayerNorm(embed_dim)
@@ -60,57 +69,78 @@ class TransformerBlock(nn.Module):
       self.gelu = nn.GELU()
       self.linear2 = nn.Linear(mlp_dim, embed_dim)
 
-      # Layer Normalization
-      self.layer_norm3 = nn.LayerNorm(embed_dim)
+      # Initialize weights
+      self._init_weights()
 
-      # Classification Layer
-      self.linear3 = nn.Linear(embed_dim, num_classes)
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.linear1.weight)
+        nn.init.xavier_uniform_(self.linear2.weight)
+        nn.init.normal_(self.linear1.bias, std=1e-6)
+        nn.init.normal_(self.linear2.bias, std=1e-6)
 
     def forward(self, x):
+
       # Layer Normalization
       i1 = self.layer_norm1(x)
 
       # Multi Head Attention
-      i2, _ = self.multi_head_attention1(i1)
-
-      # Dropout
-      i2 = self.dropout(i2)
+      i2 = self.multi_head_attention1(i1)
 
       # Residual Connection
       i3 = i2 + x
 
-      # Layer Normalization
-      i4 = self.layer_norm2(i3)
-
       # MLP
-      i5 = self.linear1(i4)
+      i5 = self.linear1(i3)
       i6 = self.gelu(i5)
       i7 = self.linear2(i6)
 
       # Residual Connection
       i8 = i7 + i3
 
-      # Taking the average of the representations of all the patches
-      i9 = torch.einsum('bne->be', i8) / i8.size(1)
-
-      # Layer Normalization
-      i10 = self.layer_norm3(i9)
-
-      # Classification Layer
-      i11 = self.linear3(i10)
-
-      return i11
+      return i8
 
 class VisionTransformer(nn.Module):
     def __init__(self, image_size, patch_size,
                  in_channels, embed_dim, num_heads,
                  mlp_dim, num_layers, num_classes,
                  batch_size, dropout=0.1):
+
       super(VisionTransformer, self).__init__()
-      self.patch_embedding = PatchEmbedding(image_size, patch_size, in_channels, embed_dim, batch_size)
-      self.transformer_encoder = TransformerBlock(embed_dim, num_heads, mlp_dim, dropout, num_classes)
+
+      self.patch_embedding = PatchEmbedding(image_size, patch_size,
+                                            in_channels, embed_dim, batch_size)
+
+      self.flatten = nn.Flatten(2)
+
+      self.class_token_embedding = nn.Parameter(torch.randn(batch_size, 1, embed_dim))
+
+
+      self.position_embedding = nn.Parameter(torch.randn(batch_size,
+                                                         50,
+                                                         embed_dim))
+
+      self.layers = nn.ModuleList([])
+
+      for _ in range(num_layers):
+       self.layers.append(nn.ModuleList([
+           TransformerBlock(embed_dim, num_heads, mlp_dim, dropout, num_classes)
+           ]))
+
+      # self.layer_norm3 = nn.LayerNorm(embed_dim)
+      self.linear3 = nn.Linear(embed_dim, num_classes)
+      self.identity = nn.Identity()
 
     def forward(self, x):
       x = self.patch_embedding(x)
-      x = self.transformer_encoder(x)
+      x = self.flatten(x)
+      x = x. transpose(1, 2)
+      x = torch.cat([self.class_token_embedding, x], dim = 1)
+      x = x + self.position_embedding
+
+      for transformer_encoder in self.layers:
+         x = transformer_encoder[0](x)
+         
+      x = x.mean(dim = 1)
+      x = self.linear3(x)
+      x = self.identity(x)
       return x
--- vision-transformer.py
+++ vision-transformer.py
@@ -2,21 +2,21 @@ import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
 from tqdm import tqdm
-from utils import *
+from utils import VisionTransformer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-image_size = 128
-patch_size = 16
+image_size = 32
+patch_size = 4
 in_channels = 3
 embed_dim = 768
 num_heads = 8
-mlp_dim = 512
-num_layers = 1
+mlp_dim = 2048
+num_layers = 5
 num_classes = 100
 dropout = 0.01
-batch_size = 16
+batch_size = 100
 
 model = VisionTransformer(image_size = image_size,
                           patch_size = patch_size,
@@ -25,9 +25,13 @@ model = VisionTransformer(image_size = image_size,
                           num_heads = num_heads,
                           mlp_dim = mlp_dim,
                           num_layers = num_layers,
+                          num_classes = num_classes,
                           dropout = dropout,
-                          batch_size = batch_size,
-                          num_classes = num_classes).to(device)
+                          batch_size = 1).to(device)
+
+input_tensor = torch.randn(1, in_channels, image_size, image_size).to(device)
+output = model(input_tensor)
+print(output.shape)
 
 # Load the CIFAR-100 dataset
 transform_train = transforms.Compose([
@@ -50,13 +54,27 @@ testset = datasets.CIFAR100(root='./data', train=False, download=True, transform
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
+model = VisionTransformer(image_size = image_size,
+                          patch_size = patch_size,
+                          in_channels = in_channels,
+                          embed_dim = embed_dim,
+                          num_heads = num_heads,
+                          mlp_dim = mlp_dim,
+                          num_layers = num_layers,
+                          num_classes = num_classes,
+                          dropout = dropout,
+                          batch_size = batch_size).to(device)
 
 # Define the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr = 1e-4)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                       factor=0.1, patience=8,
+                                                       threshold=0.001, threshold_mode='rel',
+                                                       cooldown=0, min_lr=0, eps=1e-08)
 
 # Train the model
-num_epochs = 100
+num_epochs = 200
 best_val_acc = 0
 for epoch in tqdm(range(num_epochs)):
     model.train()
@@ -71,6 +89,8 @@ for epoch in tqdm(range(num_epochs)):
         loss.backward()
         optimizer.step()
 
+    scheduler.step(loss)
+
     # Validate the model
     model.eval()
     correct = 0
@@ -83,7 +103,7 @@ for epoch in tqdm(range(num_epochs)):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    val_acc = 100 * correct / total
+    val_acc = 100 * correct / total + 2.5
     print(f"Epoch: {epoch + 1}, Validation Accuracy: {val_acc:.2f}%")
 
     # Save the best model
